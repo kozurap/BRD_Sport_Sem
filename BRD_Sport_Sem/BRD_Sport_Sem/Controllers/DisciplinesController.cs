5d12c467cecb6f2bf164c4323c7dec4ad9f7c89b
Timur Abdullin <timur.122@yandex.ru>
2020-12-16 20:33:51 +0300

    Trying to destroy the "Muzhik"

--- BRD_Sport_Sem/BRD_Sport_Sem/Controllers/DisciplinesController.cs
+++ BRD_Sport_Sem/BRD_Sport_Sem/Controllers/DisciplinesController.cs
@@ -13,5 +13,40 @@ namespace BRD_Sport_Sem.Controllers
         {
             return View("DisciplinesList");
         }
+        [Action("~/DisciplinesList/AlcoChess", Method = MethodType.GET)]
+        public IActionResult GetAlcoChess()
+        {
+            return View("AlcoChess");
+        }
+        [Action("~/DisciplinesList/BeerMarathon", Method = MethodType.GET)]
+        public IActionResult GetBeerMarathon()
+        {
+            return View("BeerMarathon");
+        }
+        [Action("~/DisciplinesList/BeerMile", Method = MethodType.GET)]
+        public IActionResult GetBeerMile()
+        {
+            return View("BeerMile");
+        }
+        [Action("~/DisciplinesList/BeerPong", Method = MethodType.GET)]
+        public IActionResult GetBeerPong()
+        {
+            return View("BeerPong");
+        }
+        [Action("~/DisciplinesList/HundredLiters", Method = MethodType.GET)]
+        public IActionResult GetHundredLiters()
+        {
+            return View("HundredLiters");
+        }
+        [Action("~/DisciplinesList/Literball", Method = MethodType.GET)]
+        public IActionResult GetLiterball()
+        {
+            return View("Literball");
+        }
+        [Action("~/DisciplinesList/WomenGlue", Method = MethodType.GET)]
+        public IActionResult GetWomenGlue()
+        {
+            return View("WomenGlue");
+        }
     }
 }
