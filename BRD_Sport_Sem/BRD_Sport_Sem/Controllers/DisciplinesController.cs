a2603381fc084b37ac9ec288a67fdd58678d387f
Nik Trush <nick_trush@mail.ru>
2020-12-16 19:49:28 +0300

    Disciplines added

---
+++ BRD_Sport_Sem/BRD_Sport_Sem/Controllers/DisciplinesController.cs
@@ -0,0 +1,17 @@
+﻿using ProjectArt.MVCPattern.Attributes;
+using Controller = ProjectArt.MVCPattern.Controller;
+using IActionResult = ProjectArt.MVCPattern.IActionResult;
+
+namespace BRD_Sport_Sem.Controllers
+{
+    [Controller("Disciplines")]
+    public class DisciplinesController : Controller
+    {
+        // GET
+        [Action("~/DisciplinesList", Method = MethodType.GET)]
+        public IActionResult Index()
+        {
+            return View("DisciplinesList");
+        }
+    }
+}
